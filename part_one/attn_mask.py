2b309e3d0d556b84f695f0d832206fd334c5f35f
viraj.awate <viraj.awate@gmail.com>
2026-05-10 16:59:43 +0530

    Added:UtilsForSingleHeadAttention

---
+++ part_one/attn_mask.py
@@ -0,0 +1,14 @@
+"""
+1.3.1 Util Script for Casual Mask for Single Head
+"""
+
+import torch
+
+def casual_mask(T: int, device = None):
+    """
+    Returns a bool mask where True means Masked
+    Shape: (1, 1, T, T) 
+    Suitable for broadcasting with (B, heads, T, T).
+    """
+    m = torch.triu(torch.ones((T, T),  dtype=torch.bool, device=device), diagonal=1)
+    return m.view(1, 1, T, T) 
