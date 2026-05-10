d208039ed58e6f039b212017e729cfe3dc18868b
viraj.awate <viraj.awate@gmail.com>
2026-05-10 15:38:15 +0530

    Editted:AddedSinPosEmbedding

--- part_one/positional_embeding.py
+++ part_one/positional_embeding.py
@@ -23,3 +23,17 @@ class LearnedPositionEncoding(nn.Module):
         pos = torch.arange(T, device=x.device)
         pose_embd = self.emb(pos)
         return x + pose_embd.unsqueeze(0)
+
+class SinusoidalPositionalEncoding(nn.Module):
+    def __init__(self, max_len: int, model_dim: int):
+        super().__init__()
+        pose_embd = torch.zeros(max_len, model_dim)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, model_dim, 2).float() * (-math.log(1e4) / model_dim))
+        pose_embd[:, 0::2] = torch.sin(position * div_term)
+        pose_embd[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer("pe", pose_embd)
+
+    def forward(self, x: torch.Tensor):
+        B, T, _ = x.shape
+        return x + self.pe[:T].unsqueeze(0)
