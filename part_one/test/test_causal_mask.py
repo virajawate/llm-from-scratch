df8f2a477bb88e46acd884b59e1de4851f1b4710
viraj.awate <viraj.awate@gmail.com>
2026-05-11 22:56:22 +0530

    Editted:CausalMaskingTestCase

--- part_one/test/test_causal_mask.py
+++ part_one/test/test_causal_mask.py
@@ -4,3 +4,10 @@ Test Case:
 
 how output of the causal mask shape
 """
+import torch
+from attn_mask import causal_mask
+
+def test_mask_is_upper_triangle():
+    m = causal_mask(5)
+    assert m.shape == (1, 1, 5, 5)
+    assert m[0,0].sum() == torch.triu(torch.ones(5,5), diagonal=1).sum()
