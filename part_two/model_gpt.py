8240eb76ad9cd8dd5fa1f540d215b60a5d206e3e
viraj.awate <viraj.awate@gmail.com>
2026-05-13 00:35:37 +0530

    Editted:AddedCausalAttentionLayer

--- part_two/model_gpt.py
+++ part_two/model_gpt.py
@@ -10,3 +10,32 @@ Main Class
     - GPT
 
 """
+from __future__ import annotations
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, n_embd: int, n_head: int, dropout: float = 0.0):
+        super().__init__()
+        assert n_embd % n_head == 0
+        self.n_head = n_head
+        self.d_head = n_embd // n_head
+        self.qkv = nn.Linear(n_embd, 3 * n_embd, bias = False)
+        self.proj = nn.Linear(n_embd, n_embd, bias=False)
+        self.dropout = nn.Dropout(dropout)
+    
+    def forward(self, x: torch.Tensor):
+        B, T, C = x.shape
+        qkv = self.qkv(x).view(B, T, 3, self.n_head, self.d_head)
+        q, k, v = qkv.unbind(dim=2)
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
+        scale = 1.0 / math.sqrt(self.d_head)
+
+        y = f.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout.p if self.training else 0.0, is_causal = True)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.proj(y)
+        return y
