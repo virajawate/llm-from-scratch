fbce1417cd93b37f49faa2a578aed81fb32f8480
viraj.awate <viraj.awate@gmail.com>
2026-05-13 01:05:46 +0530

    Editted:AddedGPTClass

--- part_two/model_gpt.py
+++ part_two/model_gpt.py
@@ -66,3 +66,61 @@ class Block(nn.Module):
         x += self.ffn(self.ln_2(x))
         return x
 
+class GPT(nn.Module):
+    def __init__(self, vocab_size: int, block_size: int, n_layer: int = 4, n_head: int = 4, n_embd: int = 256, dropout: float = 0.0):
+        super().__init__()
+        self.block_size = block_size
+        self.tok_emb = nn.Embedding(vocab_size, n_embd)
+        self.pos_emb = nn.Embedding(block_size, n_embd)
+        self.drop = nn.Dropout(dropout)
+        self.block = nn.ModuleList([Block(n_embd, n_head, dropout) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.head = nn.Linear(n_embd, vocab_size, bias=False)
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            nn.init.normal(m.weight, mean=0.0, std=0.02)
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        elif isinstance(m, nn.Embedding):
+            nn.init.normal_(m.weight, mean=0.0, std=0.02)
+    
+    def forward(self, idx: torch.Tensor, targets: torch.Tensor | None = None):
+        B, T = idx.shape
+        assert T <=self.block_size
+        pos = torch.arange(0, T, device=idx.device).unsqueeze(0)
+        x = self.tok_emb(idx) + self.pos_emb(pos)
+        x = self.drop(x)
+        for blk in self.block:
+            x = blk(x)
+        x = self.ln_f(x)
+        logits = self.head(x)
+        loss = None
+        if targets is not None:
+            loss = f.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+    
+    @torch.no_grad()
+    def generate(
+        self, 
+        idx: torch.Tensor, 
+        max_new_tokens: int = 200, 
+        temperature: float = 1.0,
+        top_k: int | None = 50,
+        top_p: float | None = None
+        ):
+        from utils import top_k_top_p_filtering
+        self.eval()
+
+        if idx.size(1) == 0:
+            idx = torch.full((idx.size(0), 1), 10, dtype=torch.long, device=idx.device)
+        for _ in range(max_new_tokens):
+            idx_cond = idx[:, -self.block_size]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :] / max(temperature, 1e-6)
+            logits = top_k_top_p_filtering(logits, top_k=top_k, top_p=top_p)
+            probs = torch.softmax(logits, dim=-1)
+            next_id = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat([idx, next_id], dim=1)
+        return idx
