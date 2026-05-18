402a67aedf7235440521a81fc8b2d363909c38a6
viraj.awate <viraj.awate@gmail.com>
2026-05-18 23:21:20 +0530

    Added:PartThreeArch

---
+++ part_three/orchestrator.py
@@ -0,0 +1,19 @@
+"""
+Part 3/
+    orchestrator.py
+    tokenizer.py
+    rmsnorm.py
+    rope.py
+    swiglu.py
+    kv_cache.py
+    attn_modern.py
+    block_modern.py
+    model_modern.py
+    demo_generate.py
+    tests/
+        test_rmsnorm.py
+        test_rope_apply.py
+        test_kvcache_shapes.py
+--------------------------------
+    
+"""
