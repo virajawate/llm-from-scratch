0fddab439c6200b4d892734dcaee72be5dd9bd23
viraj.awate <viraj.awate@gmail.com>
2026-05-19 21:12:14 +0530

    Added:RMSNormScript

---
+++ part_3/rms_norm.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class RMSNorm(nn.Module):
+    """
+    Root Mean Square Layer Normalization
+    ------------------------
+    y = x * g / rms(x)
+
+    rms(x) = sqrt(mean(x**2) + eps)
+    """
+    def __init__(self, dim:int, eps:float=1e-8):
+        super().__init__()
+        self.eps = eps
+        self.wei = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        rms = x.pow(2).mean(dim=-1, keepdim=True).add(self.eps).sqrt()
+        return (x/rms) * self.wei
