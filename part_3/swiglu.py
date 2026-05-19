0cefafc8a2fb545eb32b4093b4b65b302676e9d7
viraj.awate <viraj.awate@gmail.com>
2026-05-19 22:28:02 +0530

    Editted:SwiGLUModelNN

--- part_3/swiglu.py
+++ part_3/swiglu.py
@@ -4,3 +4,16 @@ class SwiGLU(nn.Module):
     """
     SwiGLU FFN : (xW1) (x) swish(xW2) = W3 with expansion factor 'mult'.
     """
+    def __init__(self, dim : int, multi : int = 4, dropout : float = 0.0):
+        super().__init__()
+        inner = multi * dim
+        self.w1 = nn.Linear(dim, inner, bias = False)
+        self.w2 = nn.Linear(dim, inner, bias = False)
+        self.w3 = nn.Linear(inner, dim, bias = False)
+        self.act = nn.SiLU()
+        self.drop = nn.Dropout(dropout)
+
+    def forward(self, x):
+        a = self.w1(x)
+        b = self.act(self.w2(x))
+        return self.drop(self.w3(a*b))
