a6c3ad9489806e763bd2d73c09454ed309c8d558
viraj.awate <viraj.awate@gmail.com>
2026-05-19 22:33:33 +0530

    Editted:AddedKVStruct

--- part_3/kv_cache.py
+++ part_3/kv_cache.py
@@ -2,3 +2,11 @@ from __future__ import annotations
 import torch
 from dataclasses import dataclass
 
+@property
+class KVCache:
+    k: torch.Tensor
+    v: torch.Tensor
+
+    @property
+    def T(self):
+        return self.k.size(2)
