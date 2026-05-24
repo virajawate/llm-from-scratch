c354c85db1cdb5e1831b0323b37a0bc4836afd5b
viraj.awate <viraj.awate@gmail.com>
2026-05-24 23:09:34 +0530

    Added:OrchestratorLayout

---
+++ part_4/orchestrator.py
@@ -0,0 +1,24 @@
+"""
+Repository Layout
+
+Part 4/
+    orchestrator.py
+    tokenizer_bpe.py
+    dataset_bpe.py
+    le_scheduler.py
+    amp_accum.py
+    checkpointing.py
+    logger.py
+    train.py
+    sample.py
+    tests/
+        test_tokenizer.py
+        test_scheduler.py
+        test_resume_shapes.py
+----
+Run inside part_4:
+    cd part_4
+    python orchestrator.py --demo
+    pytest -q
+    tensorboard --logdir=runs/part4-demo
+"""
