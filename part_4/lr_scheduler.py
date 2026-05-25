6988b646ee9d7983fd00cdb692bde49a5da05fd8
viraj.awate <viraj.awate@gmail.com>
2026-05-25 23:11:19 +0530

    Added:LrScheduler

---
+++ part_4/lr_scheduler.py
@@ -0,0 +1,11 @@
+import math
+
+class WarmupCosineLR:
+    """
+    Linear warmup -> cosine decay (per0step API).
+    """
+    def __init__(self):
+        pass
+    
+    def step(self):
+        pass
