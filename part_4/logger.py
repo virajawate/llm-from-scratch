182b9bab65d4ce2ff9a612914859df377a1941a7
viraj.awate <viraj.awate@gmail.com>
2026-05-29 22:49:01 +0530

    Editted:Constuctor&LogFn

--- part_4/logger.py
+++ part_4/logger.py
@@ -24,10 +24,54 @@ class TBLogger(NoopLogger):
         - If key starts with "text/", logs as text.
     """
     def __init__(self, output_dir:str, flush_secs:int=10, run_name:str|None=None):
-        pass
+        self.w = None
+        self.hparams_logged = False
+        run_name = run_name or time.strftime("%Y%m%d - %H%M%S")
+        run_dir = Path(output_dir) / run_name
+        run_dir.mkdir(parents=True, exist_ok=True)
+        try:
+            from torch.utils.tensorboard import SummaryWriter
+            self.w = SummaryWriter(log_dir=str(run_dir), flush_secs=flush_secs)
+        except Exception as e:
+            print(f"[TBLogger] TensorBoard nto available : {e}. Logging disabled.")
+        self._auto_hist_max_elems = 2048
+        self.run_dir = str(run_dir)
 
     def log(self, step:Optional[int] = None, **kv:Any):
-        pass
+        if not self.w: return
+        for k, v in kv.items():
+            if isinstance(k, str) and k.startswith("text/"):
+                try:
+                    self.w.add_text(k[5:], str(v), global_step=step)
+                except Exception:
+                    pass
+                continue
+
+            try:
+                import torch, numpy as np
+                is_torch = isinstance(v, torch.Tensor)
+                is_np = isinstance(v, np.ndarray)
+                if is_torch or is_np:
+                    numel = int(v.numel() if is_torch else v.size)
+                    if numel == 1:
+                        val = (v.item() if is_torch else float(v))
+                        self.w.add_scalar(k, float(val), global_step=step)
+                    else:
+                        if numel <= self._auto_hist_max_elems:
+                            self.w.add_histogram(k, v.detach().cpu() if is_torch else v, global_step=step)
+                        else:
+                            arr = v.detach().cpu().flatten().numpy() if is_torch else v.flatten()
+                            self.w.add_scalar(k + "/mean", float(arr.mean()), global_step=step)
+                            self.w.add_scalar(k + "/std", float(arr.std()), global_step=step)
+                    continue
+            except Exception:
+                pass
+
+            try:
+                self.w.add_scalar(k, float(v), global_step=step)
+            except Exception:
+                pass
+
 
     def hist(self, tag:str, values:Any, step:Optional[int]=None, bins:str="tensorflow"):
         pass
