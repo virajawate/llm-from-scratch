8ced5b6cf08aa1d00b48de28806765ced23422a4
viraj.awate <viraj.awate@gmail.com>
2026-05-30 23:49:53 +0530

    Editted:AddedLossEstimationFn

--- part_4/train.py
+++ part_4/train.py
@@ -4,3 +4,17 @@ import torch
 from tokenizer import ByteTokenizer
 from dataset import ByteDataset
 from model_gpt import GPT
+
+def estimate_loss(model: GPT, ds: ByteDataset, args)->dict:
+    model.eval()
+    output = {}
+    with torch.no_grad():
+        for split in ["trian", "val"]:
+            losses = []
+            for _ in range(args.eval_iters):
+                xb, yb = ds.get_batch(split, args.batch_size, args.device)
+                _, loss = model(xb, yb)
+                losses.append(loss.item())
+            output[split] = sum(losses) / len[losses]
+    model.train()
+    return output
