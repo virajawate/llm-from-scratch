035873faadb712fc4e6ddc30b9d66833a7b8d564
viraj.awate <viraj.awate@gmail.com>
2026-05-31 19:46:33 +0530

    Added:TestCaseForBPETokenizer

---
+++ part_4/tests/test_tokenizer_bpe.py
@@ -0,0 +1,16 @@
+import os, tempfile
+from tokenizer_bpe import BPETokenizer
+
+def test_bpe_train_save_load_roundtrip():
+    with tempfile.TemporaryDirectory() as d:
+        txt = os.path.join(d, 'tiny.txt')
+        with open(txt, 'w') as f:
+            f.write("Hello Hello World")
+        tok = BPETokenizer(vocab_size=100)
+        tok.train(txt)
+        output = os.path.join(d, 'tok')
+        tok.save(output)
+        tok2 = BPETokenizer()
+        tok2.load(output)
+        ids = tok2.encode('Hellow World')
+        assert isinstance(ids, list) and len(ids) > 0
