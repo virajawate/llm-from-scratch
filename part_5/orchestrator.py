7262e605436bd83210e3fb6665e7ea63d7ac70b9
viraj.awate <viraj.awate@gmail.com>
2026-06-02 23:15:30 +0530

    Added:orchestratorScriptAndStructure

---
+++ part_5/orchestrator.py
@@ -0,0 +1,20 @@
+"""
+Part_5/
+    orchestrator.py
+    README_PART5_OUTPUT.md
+    gating.py
+    moe.py
+    block_hybrid.py
+    demo_moe.py
+    tests/
+        test_gate_shapes.py
+        test_moe_forward.py
+        test_hybrid_block.py
+
+Run from part_5/
+---
+cd part_5
+python orchestrator.py --demo
+pytest -q
+
+"""
