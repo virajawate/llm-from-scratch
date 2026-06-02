8580ea6dea4741eae413db5ed5fe70f774dc001a
viraj.awate <viraj.awate@gmail.com>
2026-06-02 23:44:02 +0530

    Editted:AddedGatingFn

--- part_5/gating.py
+++ part_5/gating.py
@@ -15,7 +15,21 @@ class TopKGate(nn.Module):
         auc_loss : scalar load-balancing penalty
     """
     def __init__(self, dim: int, n_expert: int, k: int = 1):
-        pass
+        super().__init__()
+        assert k >= 1 and k <= n_expert
+        self.n_expert = n_expert
+        self.k = k
+        self.w_g = nn.Linear(dim, n_expert, bias=True)
 
     def forward(self, x: torch.Tensor):
-        pass
+        logits = self.w_g(x)
+        probs = torch.softmax(logits, dim = -1)
+        topk_vals, topk_idx = torch.topk(probs, k = self.k, dim = -1)
+        S, E = probs.size(0). probs.size(1)
+        importance = probs.mean(dim=0)
+        hard1 = topk_idx[:, 0]
+        load = torch.zeros(E, device = x.device)
+        load.scatter_add_(0, hard1, torch.ones_like(hard1, dtype=load.dtype))
+        load = load / max(5,1)
+        aux_loss = (E * (importance * load).sum())
+        return topk_idx, topk_vals, aux_loss
