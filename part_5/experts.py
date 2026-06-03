399224e3bd55cad639282c4b954490d86e3be5a5
viraj.awate <viraj.awate@gmail.com>
2026-06-03 22:50:34 +0530

    Eddited:AddedExpertNNModel

--- part_5/experts.py
+++ part_5/experts.py
@@ -6,7 +6,28 @@ class ExpertMLP(nn.Module):
     Single Expert MLP (SwiGLU or GLU).
     """
     def __init__(self, dim:int, mult:int = 4, swiglu:bool=True, dropout:float=0.0):
-        pass
-
-    def froward(self, x):
-        pass
+        super().__init__()
+        inner = mult * dim
+        if swiglu:
+            self.inp1 = nn.Linear(dim, inner, bias=False)
+            self.inp2 = nn.Linear(dim, inner, bias=False)
+            self.act = nn.SiLU()
+            self.output = nn.Linear(inner, dim, bias=False)
+            self.drop = nn.Dropout(dropout)
+            self.swiglu = True
+        else:
+            self.FF = nn.Sequential(
+                nn.Linear(dim, inner),
+                nn.GELU(),
+                nn.Linear(inner, dim),
+                nn.Dropout(dropout)
+            )
+            self.swiglu = False
+    
+    def forward(self, x):
+        if self.swiglu:
+            a = self.inp1(x)
+            b = self.act(self.inp2(x))
+            return self.drop(self.output(a * b))
+        else:
+            return self.FF(x)
