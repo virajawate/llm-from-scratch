f364d83a558f0cceb4009171983769206d5b6ca1
viraj.awate <viraj.awate@gmail.com>
2026-06-06 23:10:13 +0530

    Added:OrchetratorScript

---
+++ part_6/orchestrator.py
@@ -0,0 +1,20 @@
+"""
+part_6/
+    - []orchestrator.py
+    - []formatters.py
+    - []dataset_sft.py
+    - []collator_sft.py
+    - []curriculum.py
+    - []evaluate.py
+    - []train_sft.py
+    - []sample_sft.py
+    - tests/
+        - []test_formatter.py
+        - []test_making.py
+
+Run from inside part_6:
+    - cd part_6
+    - python orchestrator.py --demo
+    - pytest -q
+
+"""
