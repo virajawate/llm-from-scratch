726aebc9398004c717b623b6e42700d180074893
viraj.awate <viraj.awate@gmail.com>
2026-06-08 22:14:35 +0530

    Editted:AddedLengthCurriculumPropeties

--- part_6/curriculum.py
+++ part_6/curriculum.py
@@ -5,11 +5,15 @@ class LengthCurriculum:
     """
     6.3 Curriculum iterate examples from short -> long prompts  (one pass demo.)
     """
-    def __init__(self):
-        pass
-
+    def __init__(self, items: List[tuple[str, str]]):
+        self.items = sorted(items, key = lambda p : len(p[0]))
+        self._i = 0
     def __iter__(self):
-        pass
-
+        self._i = 0
+        return self
     def __next__(self):
-        pass
+        if self._i >= len(self.items):
+            raise StopIteration
+        it = self.items[self._i]
+        self._i += 1
+        return it
