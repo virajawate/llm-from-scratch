a64e8d866a2cf84ab7caefa21680cdb828b740f4
viraj.awate <viraj.awate@gmail.com>
2026-06-08 22:11:56 +0530

    Added:LengthCurriculumClass

---
+++ part_6/curriculum.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+from typing import List
+
+class LengthCurriculum:
+    """
+    6.3 Curriculum iterate examples from short -> long prompts  (one pass demo.)
+    """
+    def __init__(self):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
