49b1914e1f7fade34572cd54468926b2ae385257
viraj.awate <viraj.awate@gmail.com>
2026-06-11 23:54:10 +0530

    Added:OrchestratorForPart7

---
+++ part_7/orchestrator.py
@@ -0,0 +1,17 @@
+"""
+Part_7/
+    - [] orchestrator.py
+    - [] data_prefs.py
+    - [] collator_rm.py
+    - [] model_reward.py
+    - [] train_rm.py
+    - [] eval_rm.py
+    - tests/
+        - [] 
+        - [] 
+
+Run the orchestrator
+cd part_7
+python orchestrator.py --demo
+pytest -q
+"""
