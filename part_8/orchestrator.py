4eb23378ec3f892174e0a8f85914ae5bef2fc8af
viraj.awate <viraj.awate@gmail.com>
2026-06-21 23:52:39 +0530

    Edited:AddedList

--- part_8/orchestrator.py
+++ part_8/orchestrator.py
@@ -1,10 +1,18 @@
 """
     Structure
     part_8/
-    - []
+    - [] orchestrator.py
+    - [] policy.py
+    - [] rollout.py
+    - [] ppo_loss.py
+    - [] train_ppo.py
+    - [] eval_ppo.py
+    - tests/
+        - [] test_ppo_loss.py
+        - [] test_policy_forward.py
 
 ---- Run
 > cd part_8
 > python orchestrator.py --demo
-
+> pytest -q
 """
